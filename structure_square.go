60ed6cdb415c0e89423f12459cf5ae98f9d6021a
Gurgen Torosyan <gurgen.torosyan@exio.tech>
2022-05-24 14:43:22 +0400

    Square

---
+++ structure_square.go
@@ -0,0 +1,36 @@
+package structure_square
+
+import (
+	"fmt"
+)
+
+type Point struct {
+	x, y int
+}
+
+type Square struct {
+	startPoint1, endPoint1, startPoint2, endPoint2 Point
+}
+func NewPoint(x, y int) Point{
+	return Point{x, y}
+}
+func NewSquare(startPoint1 Point) (s Square) {
+	s.startPoint1 = startPoint1
+	s.endPoint1.x = s.startPoint1.x
+	s.endPoint1.y = -s.startPoint1.y
+	s.startPoint2.x = 2 * s.startPoint1.y
+	s.startPoint2.y = s.startPoint1.y
+	s.endPoint2.x = s.startPoint2.x
+	s.endPoint2.y = -s.startPoint2.y
+	return s
+}
+
+func (s *Square) area() int {
+	side := s.startPoint2.x - s.startPoint1.x
+	return side * side
+}
+
+func (s *Square) perimeter() int {
+	side := s.startPoint2.x - s.startPoint1.x
+	return side * 4
+}
